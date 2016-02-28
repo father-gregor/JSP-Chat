682113c806f70b41f7c7e2177a5601ada6a8bdb6
father-gregor <benlinus92@gmail.com>
2016-02-28 13:41:21 +0200

    Change persistence.xml config

--- src/main/java/ua/com/benlinus92/server/DatabaseSQL.java
+++ src/main/java/ua/com/benlinus92/server/DatabaseSQL.java
@@ -6,7 +6,6 @@ import javax.persistence.Persistence;
 public class DatabaseSQL {
 	private static final DatabaseSQL dsql = new DatabaseSQL();
 	private static EntityManagerFactory emf;
-	private static final String DB_NAME = "JSPchat";
 	
 	private DatabaseSQL() { }
 	
